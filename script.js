056705e38eb61a12c609a2412dccb8ca0e13b01e
smloring <156940158+smloring@users.noreply.github.com>
2024-02-18 22:34:25 +0000

    lab 6 submitted

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function draw() {
   ufo(x, y, d, r); // call the ufo() function (see below)
   
   // add code here to change the ship location each frame
-  
-  
+  x++;
+  r +=.01;
 }
 
 function ufo(tempX, tempY, tempD, tempR){
